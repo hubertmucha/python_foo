dae4158887dbc050a615443b42142a218ff760ec
Hubert Mucha <58261346+hubertmucha@users.noreply.github.com>
2021-01-19 19:43:31 +0100

    Create f02_15_string2charList.py

---
+++ f02_15_string2charList.py
@@ -0,0 +1,21 @@
+def f02_15_string2charList(s):
+    """
+    Converts the string into the list of chars
+
+    Parameters
+    ----------
+    s : str
+        Any string.
+    
+    Returns
+    -------
+    list
+        list of characters of the input string
+        
+    Implementation constraints
+    --------------------------
+    - target number of non-white characters for the code: 42
+      (docstring and comments are ignored when counting)
+    - do not use "import"
+    """
+    return list(s)
