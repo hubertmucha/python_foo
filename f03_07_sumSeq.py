70fbea8b01842397bd7313d75168f87184230b86
Hubert Mucha <58261346+hubertmucha@users.noreply.github.com>
2021-01-27 19:06:17 +0100

    Create f03_07_sumSeq.py

---
+++ f03_07_sumSeq.py
@@ -0,0 +1,26 @@
+def f03_07_sumSeq(a,n):
+    """
+    Calculates the sum of consecutive numbers in the list.
+
+    Parameters
+    ----------
+    a : list
+        List of numbers.
+    n : int
+        Number of elements to be summed.
+
+    Returns
+    -------
+    list
+        List of numbers. Each number is a sum of n consecutive
+        input list members, e.g.:
+            [1,2,3,4,5], 2  ->  [3,5,7,9]
+            [1,2,3,4,5], 3  ->  [6,9,12]
+            
+    Implementation constraints
+    --------------------------
+    - target number of non-white characters for the code: 70
+      (docstring and comments are ignored when counting)
+    - do not use: "import"
+    """
+    return [sum(a[i:i+n]) for i in range(len(a)-n+1)]
